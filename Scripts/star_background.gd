1fa6cbdc5a60b33b656cd23ab42f1d6bc0f664e9
GANONDORF\CASPI <caspian@tylerinternet.com>
2025-07-31 18:27:48 -0400

    FINALLY FOUND A STUPID SHADER THAT DOES EXACTLY WHAT I WANTED

--- Scripts/star_background.gd
+++ Scripts/star_background.gd
@@ -1,9 +1,9 @@
-extends TextureRect
+extends Control
 class_name StarBackground
 
 	
 func _process(_delta: float) -> void:
-	material.set("shader_parameter/offset", Player.Position * 0.0001)
+	material.set("shader_parameter/offset", Player.Position)
 	#material.set("shader_parameter/paralax", amount)
 	return
 
